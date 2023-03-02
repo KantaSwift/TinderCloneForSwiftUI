69e460aa4e07dde2129e9b56fcd6106ed56d0527
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-03-02 17:56:39 +0900

    first commit

---
+++ TinderCloneForSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TinderCloneForSwiftUI
+//
+//  Created by 上條栞汰 on 2023/03/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinderCloneForSwiftUI/TinderCloneForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneForSwiftUIApp.swift
+//  TinderCloneForSwiftUI
+//
+//  Created by 上條栞汰 on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
